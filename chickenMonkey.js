d1e2ae9078028be174a67f79a87d792d182fc042
Allie <allison.tpttn@gmail.com>
2019-07-15 13:58:47 -0500

    working chickenMonkey files

---
+++ chickenMonkey.js
@@ -0,0 +1,13 @@
+
+for (let currentNumber = 1; currentNumber <= 100; currentNumber++) {
+    // Divide the current number by 5, and check if the remainder is 0
+    if (currentNumber % 5 === 0) {
+        console.log("chicken")
+    }
+    if (currentNumber % 7 === 0) {
+        console.log("Monkey")
+    } 
+    if (currentNumber % 5 === 0 || currentNumber % 7 === 0) {
+        console.log("ChickenMonkey")
+    }
+}
